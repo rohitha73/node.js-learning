73744040c077ffc82891635d745aa123b33f4913
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2025-02-01 22:51:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import {randomSuperhero} from 'superheroes';
+randomSuperhero();
+
+console.log('My Name is '+ randomSuperhero());
+
+//var generateName = require('sillyname');
+
+/*import generateName from 'sillyname';
+var sillyName = generateName();
+
+console.log('My name is ' + sillyName);*/
